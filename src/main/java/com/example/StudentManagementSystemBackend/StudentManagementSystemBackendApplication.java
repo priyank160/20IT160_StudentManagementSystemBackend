933247711ddf37a3fd03f202784ab0a20a787a7d
priyank160 <88378991+priyank160@users.noreply.github.com>
2022-09-01 13:06:41 +0530

    Added GET, DELETE, PUT, POST mappings, and Student class with dummy data

---
+++ src/main/java/com/example/StudentManagementSystemBackend/StudentManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemBackendApplication.class, args);
+	}
+
+}
