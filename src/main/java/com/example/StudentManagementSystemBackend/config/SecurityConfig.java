2087947d0fdd99119a12c4ae05f791d11afe18c1
priyank160 <88378991+priyank160@users.noreply.github.com>
2022-09-12 12:18:36 +0530

    Cors filer is added to handle axios request.

--- src/main/java/com/example/StudentManagementSystemBackend/config/SecurityConfig.java
+++ src/main/java/com/example/StudentManagementSystemBackend/config/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.example.StudentManagementSystemBackend.config;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
